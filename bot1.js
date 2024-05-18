c91fdd39d9d840a3b83cecbc3b3a89bba31ef53e
IceVladimir <74771948+IceVladimir@users.noreply.github.com>
2024-05-18 19:57:28 +0300

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -30,7 +30,7 @@ app.post('/', function(req, res) {
 	});
   }
   
-  if (arr_of_puppets.size >= 100){
+  if (arr_of_puppets.size >= 80){
 	res.send({
 		'Answer': "{servers are overloaded, please wait}",
 		'Token': req.body.token,
