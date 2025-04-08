d75004cde9909af38e5511b68a0da0c99986dbc5
IceVladimir <74771948+IceVladimir@users.noreply.github.com>
2025-04-08 14:05:47 +0300

    little update

--- bot1.js
+++ bot1.js
@@ -5,6 +5,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const { CharacterAI } = require("node_characterai");
+const accounts = Array("35ffa2332d6523a45833a1fd3a73bb6f2b2febdd", "a74fcf11914d2f24b71bf2de303188d1c883dce0", "a11d77cad2f24263277cca8ac9315eeaa01700fd", "27ecfe97bc56402020659909c776172763dcb1f3");
 
 const arr_of_puppets = new Map();
 const {v4 : uuidv4} = require('uuid')
@@ -51,7 +52,7 @@ app.post('/', function(req, res) {
 	return;
   }
   
-  var account = "35ffa2332d6523a45833a1fd3a73bb6f2b2febdd";
+  var account = accounts[Math.floor(Math.random()*accounts.length)];
   var client_key = getEmptyClient()
   
   if (client_key == false){
@@ -74,8 +75,10 @@ app.post('/', function(req, res) {
   
   if (req.body.variant == 1){
 	  characterId = "mXzyPFgxh0IMoydznW110YnhiHF6ITNfiKAGklhsrjU";
-  }else{  
+  }else if (req.body.variant == 2){  
 	  characterId = "roCAnDLY3GUGRwUS1iR_GncjvxvntJtdGFsDZGtPMBo";
+  }else if (req.body.variant == 3){  
+	  characterId = "5g8yBVUX2cJ7dxUTjEG8oSUUU4uzg-gn4Dy-Y57XSno";
   }
   
   try {
