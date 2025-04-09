b070ce0b837f4ec6dcb2c90724e07e02258c5c73
IceVladimir <74771948+IceVladimir@users.noreply.github.com>
2025-04-09 20:49:44 +0300

    new update

--- bot1.js
+++ bot1.js
@@ -6,29 +6,41 @@ app.use(express.urlencoded({ extended: true }));
 
 const { CharacterAI } = require("node_characterai");
 const accounts = Array("35ffa2332d6523a45833a1fd3a73bb6f2b2febdd", "a74fcf11914d2f24b71bf2de303188d1c883dce0", "a11d77cad2f24263277cca8ac9315eeaa01700fd", "27ecfe97bc56402020659909c776172763dcb1f3");
-
-const arr_of_puppets = new Map();
 const {v4 : uuidv4} = require('uuid')
+//const characterAI = new CharacterAI();
+
+
+let characterV1;
+let characterV2;
+let characterV3;
+var AccountAuth = new Map();
+var AccountChars = new Map();
+var ServerStatus = false;
 
 async function StartClients() {
-	var os = require('os-utils');
-	//while (os.freemem() >= 4000){
-	for (let i = 0; i < 50; i++){
-		arr_of_puppets.set(uuidv4(), [new CharacterAI(), false, false]);
+	for (index = 0; index < accounts.length; ++index) {
+		let accountID = accounts[index];
+		
+		let charAI = new CharacterAI();
+		await charAI.authenticate(accounts[index])
+		AccountAuth.set(accountID, charAI);
+		
+		let arr_of_chars = Array();
+		arr_of_chars.push(await AccountAuth.get(accounts[index]).fetchCharacter("mXzyPFgxh0IMoydznW110YnhiHF6ITNfiKAGklhsrjU"))
+		arr_of_chars.push(await AccountAuth.get(accounts[index]).fetchCharacter("roCAnDLY3GUGRwUS1iR_GncjvxvntJtdGFsDZGtPMBo"))
+		arr_of_chars.push(await AccountAuth.get(accounts[index]).fetchCharacter("5g8yBVUX2cJ7dxUTjEG8oSUUU4uzg-gn4Dy-Y57XSno"))
+		
+		AccountChars.set(accountID, arr_of_chars);
 	}
-	console.log(arr_of_puppets);
+	ServerStatus = true;
 }
-
-function getEmptyClient() {
-	for (var entry of arr_of_puppets.entries()) {
-		var key = entry[0],
-        value = entry[1];
-		if (value[1] == false){
-			return key
-		}
-	}
-    return false
-}
+/*
+characterAI.authenticate("35ffa2332d6523a45833a1fd3a73bb6f2b2febdd").then(async() => {
+    characterV1 = await characterAI.fetchCharacter("mXzyPFgxh0IMoydznW110YnhiHF6ITNfiKAGklhsrjU");
+	characterV2 = await characterAI.fetchCharacter("roCAnDLY3GUGRwUS1iR_GncjvxvntJtdGFsDZGtPMBo");
+	characterV3 = await characterAI.fetchCharacter("5g8yBVUX2cJ7dxUTjEG8oSUUU4uzg-gn4Dy-Y57XSno");
+});
+*/
 
 app.get('/', (req, res) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
@@ -42,74 +54,48 @@ app.get('/usage', (req, res) => {
 
 app.post('/', function(req, res) {
 (async () => {
-  var os = require('os-utils');
- if (os.freemem() <= 700.0){
+  
+  if(ServerStatus == false){
 	res.send({
-		'Answer': "{servers are overloaded, please wait}",
+		'Answer': "Server starting, please wait!",
 		'Token': req.body.token,
 		'Account': req.body.account,
-	});
-	return;
+  });
   }
+	
+	
+  let chat;
+  let character;
+  let account;
   
-  var account = accounts[Math.floor(Math.random()*accounts.length)];
-  var client_key = getEmptyClient()
-  
-  if (client_key == false){
-	  res.send({
-		'Answer': "{servers are overloaded, please wait}",
-		'Token': req.body.token,
-		'Account': req.body.account,
-	});
-	return;
-  }
-  arr_of_puppets.get(client_key)[1] = true
-  
-  if (arr_of_puppets.get(client_key)[2] == false){
-	await arr_of_puppets.get(client_key)[0].authenticate(account);
-	arr_of_puppets.get(client_key)[2] = true
-  }
-  
-  
-  var characterId;
-  
-  if (req.body.variant == 1){
-	  characterId = "mXzyPFgxh0IMoydznW110YnhiHF6ITNfiKAGklhsrjU";
-  }else if (req.body.variant == 2){  
-	  characterId = "roCAnDLY3GUGRwUS1iR_GncjvxvntJtdGFsDZGtPMBo";
-  }else if (req.body.variant == 3){  
-	  characterId = "5g8yBVUX2cJ7dxUTjEG8oSUUU4uzg-gn4Dy-Y57XSno";
-  }
-  
-  try {
-	  
-  const character = await arr_of_puppets.get(client_key)[0].fetchCharacter(characterId);
-  var dm;
-  var new_token = {};
-  
-  if(req.body.token != ""){
-	  dm = await character.DM(req.body.token);
+  if (req.body.account == ""){
+	  account = accounts[Math.floor(Math.random()*accounts.length)];
   }else{
-	  dm = await character.createDM(); 
-  }	
-  
-  response = await dm.sendMessage(req.body.msg);
-  new_token = response["chatId"]
-  if (response.content) response = response.content
-  if (response == "Error or Violent content") {
-	  arr_of_puppets.get(client_key)[2] = false;
-	  arr_of_puppets.get(client_key)[0].unauthenticate();
+	  account = req.body.account;
   }
-  arr_of_puppets.get(client_key)[1] = false
-  } catch (err) {
-	  arr_of_puppets.get(client_key)[1] = false;
-	  arr_of_puppets.get(client_key)[2] = false;
-	  arr_of_puppets.get(client_key)[0].unauthenticate();
+  character = AccountChars.get(account)[req.body.variant - 1]
+  
+  /*
+  if (req.body.variant == 1){
+	  character = characterV1;
+  }else if (req.body.variant == 2){  
+	  character = characterV2;
+  }else if (req.body.variant == 3){  
+	  character = characterV3;
+  }
+  */
+  
+  if(req.body.token == ""){
+	  chat = await character.createDM();
+  }else{
+	  chat = await character.DM(req.body.token);
   }
  
+  const message = await chat.sendMessage(req.body.msg);
+  
   res.send({
-    'Answer': response,
-	'Token': new_token,
+    'Answer': message.content,
+	'Token': message.chatId,
 	'Account': account,
   });
 
