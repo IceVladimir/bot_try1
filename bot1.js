7ef836dffde4d62e696ffc9d6cf193c7c5ee1966
IceVladimir <74771948+IceVladimir@users.noreply.github.com>
2025-04-09 21:03:04 +0300

    asd

--- bot1.js
+++ bot1.js
@@ -7,7 +7,6 @@ app.use(express.urlencoded({ extended: true }));
 const { CharacterAI } = require("node_characterai");
 const accounts = Array("35ffa2332d6523a45833a1fd3a73bb6f2b2febdd", "a74fcf11914d2f24b71bf2de303188d1c883dce0", "a11d77cad2f24263277cca8ac9315eeaa01700fd", "27ecfe97bc56402020659909c776172763dcb1f3");
 const {v4 : uuidv4} = require('uuid')
-//const characterAI = new CharacterAI();
 
 var AccountAuth = new Map();
 var AccountChars = new Map();
@@ -30,13 +29,6 @@ async function StartClients() {
 	}
 	ServerStatus = true;
 }
-/*
-characterAI.authenticate("35ffa2332d6523a45833a1fd3a73bb6f2b2febdd").then(async() => {
-    characterV1 = await characterAI.fetchCharacter("mXzyPFgxh0IMoydznW110YnhiHF6ITNfiKAGklhsrjU");
-	characterV2 = await characterAI.fetchCharacter("roCAnDLY3GUGRwUS1iR_GncjvxvntJtdGFsDZGtPMBo");
-	characterV3 = await characterAI.fetchCharacter("5g8yBVUX2cJ7dxUTjEG8oSUUU4uzg-gn4Dy-Y57XSno");
-});
-*/
 
 app.get('/', (req, res) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
@@ -50,8 +42,9 @@ app.get('/usage', (req, res) => {
 
 app.post('/', function(req, res) {
 (async () => {
-  
-  if(ServerStatus == false || AccountAuth.length != accounts.length){
+  console.log(ServerStatus, AccountAuth.length, accounts.length)
+  var cantUse = (ServerStatus == false || AccountAuth.length != accounts.length)
+  if(cantUse){
 	res.send({
 		'Answer': "Server starting, please wait!",
 		'Token': req.body.token,
@@ -59,10 +52,11 @@ app.post('/', function(req, res) {
   });
   }
 	
-	
-  var chat;
-  var character;
-  var account;
+  if (!cantUse){
+
+  let chat;
+  let character;
+  let account;
   
   if (req.body.account == ""){
 	  account = accounts[Math.floor(Math.random()*accounts.length)];
@@ -70,16 +64,6 @@ app.post('/', function(req, res) {
 	  account = req.body.account;
   }
   character = AccountChars.get(account)[req.body.variant - 1]
-  
-  /*
-  if (req.body.variant == 1){
-	  character = characterV1;
-  }else if (req.body.variant == 2){  
-	  character = characterV2;
-  }else if (req.body.variant == 3){  
-	  character = characterV3;
-  }
-  */
   
   if(req.body.token == ""){
 	  chat = await character.createDM();
@@ -94,31 +78,10 @@ app.post('/', function(req, res) {
 	'Token': message.chatId,
 	'Account': account,
   });
-
+  }
 })();
 });
 app.listen(port, () =>{
 	console.log('server started on ' + process.env.DOMAIN + ":" + port)
 	StartClients();
 })
-
-/*
-(async () => {
-  // Authenticating as a guest (use `.authenticateWithToken()` to use an account)
-  await characterAI.authenticate("35ffa2332d6523a45833a1fd3a73bb6f2b2febdd").then(async() => {
-	   console.log("Logged in");
-	   // start coding in here!
-  });
-  
-  const character = await characterAI.fetchCharacter("mXzyPFgxh0IMoydznW110YnhiHF6ITNfiKAGklhsrjU");
-  const dm = await character.DM(); 
-
-// send it a message
-  const message = await dm.sendMessage("hello");
-
-// get the text content
-  const content = message.content;
-
-  console.log(message);	
-})();
-*/
