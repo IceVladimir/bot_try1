6f61196e353e56fa2cbf1feeef21e9f9f984c56c
IceVladimir <74771948+IceVladimir@users.noreply.github.com>
2025-04-10 00:54:01 +0300

    update

--- bot1.js
+++ bot1.js
@@ -6,28 +6,27 @@ app.use(express.urlencoded({ extended: true }));
 
 const { CharacterAI } = require("node_characterai");
 const accounts = Array("35ffa2332d6523a45833a1fd3a73bb6f2b2febdd", "a74fcf11914d2f24b71bf2de303188d1c883dce0", "a11d77cad2f24263277cca8ac9315eeaa01700fd", "27ecfe97bc56402020659909c776172763dcb1f3");
+
+const arr_of_puppets = new Map();
 const {v4 : uuidv4} = require('uuid')
 
-var AccountAuth = new Map();
-var AccountChars = new Map();
-var ServerStatus = false;
-
 async function StartClients() {
-	for (index = 0; index < accounts.length; ++index) {
-		let accountID = accounts[index];
-		
-		let charAI = new CharacterAI();
-		await charAI.authenticate(accounts[index])
-		AccountAuth.set(accountID, charAI);
-		
-		let arr_of_chars = Array();
-		arr_of_chars.push(await AccountAuth.get(accounts[index]).fetchCharacter("mXzyPFgxh0IMoydznW110YnhiHF6ITNfiKAGklhsrjU"))
-		arr_of_chars.push(await AccountAuth.get(accounts[index]).fetchCharacter("roCAnDLY3GUGRwUS1iR_GncjvxvntJtdGFsDZGtPMBo"))
-		arr_of_chars.push(await AccountAuth.get(accounts[index]).fetchCharacter("5g8yBVUX2cJ7dxUTjEG8oSUUU4uzg-gn4Dy-Y57XSno"))
-		
-		AccountChars.set(accountID, arr_of_chars);
+	var os = require('os-utils');
+	for (let i = 0; i < 50; i++){
+		arr_of_puppets.set(uuidv4(), [new CharacterAI(), false, false]);
 	}
-	ServerStatus = true;
+	console.log(arr_of_puppets);
+}
+
+function getEmptyClient() {
+	for (var entry of arr_of_puppets.entries()) {
+		var key = entry[0],
+        value = entry[1];
+		if (value[1] == false){
+			return key
+		}
+	}
+    return false
 }
 
 app.get('/', (req, res) => {
@@ -42,42 +41,75 @@ app.get('/usage', (req, res) => {
 
 app.post('/', function(req, res) {
 (async () => {
-  console.log(ServerStatus)
-  if(ServerStatus == false){
+  var os = require('os-utils');
+ if (os.freemem() <= 700.0){
 	res.send({
-		'Answer': "Server starting, please wait!",
+		'Answer': "{servers are overloaded, please wait}",
 		'Token': req.body.token,
 		'Account': req.body.account,
-  });
+	});
+	return;
   }
-	
-  if (ServerStatus == true){
-
-  let chat;
-  let character;
-  let account;
+  
+  var account;
   
   if (req.body.account == ""){
 	  account = accounts[Math.floor(Math.random()*accounts.length)];
   }else{
 	  account = req.body.account;
   }
-  character = AccountChars.get(account)[req.body.variant - 1]
   
-  if(req.body.token == ""){
-	  chat = await character.createDM();
-  }else{
-	  chat = await character.DM(req.body.token);
+  var client_key = getEmptyClient()
+  
+  if (client_key == false){
+	  res.send({
+		'Answer': "{servers are overloaded, please wait}",
+		'Token': req.body.token,
+		'Account': req.body.account,
+	});
+	return;
   }
- 
-  const message = await chat.sendMessage(req.body.msg);
+  arr_of_puppets.get(client_key)[1] = true
   
+  if (arr_of_puppets.get(client_key)[2] == false){
+	await arr_of_puppets.get(client_key)[0].authenticate(account);
+	arr_of_puppets.get(client_key)[2] = true
+  }
+  
+  
+  var characterId;
+  
+  if (req.body.variant == 1){
+	  characterId = "mXzyPFgxh0IMoydznW110YnhiHF6ITNfiKAGklhsrjU";
+  }else if (req.body.variant == 2){  
+	  characterId = "roCAnDLY3GUGRwUS1iR_GncjvxvntJtdGFsDZGtPMBo";
+  }else if (req.body.variant == 3){  
+	  characterId = "5g8yBVUX2cJ7dxUTjEG8oSUUU4uzg-gn4Dy-Y57XSno";
+  }
+	  
+  const character = await arr_of_puppets.get(client_key)[0].fetchCharacter(characterId);
+  var dm;
+  var new_token = {};
+  
+  if(req.body.token != ""){
+	  dm = await character.DM(req.body.token);
+  }else{
+	  dm = await character.createDM(); 
+  }	
+  
+  response = await dm.sendMessage(req.body.msg);
+  new_token = response.chatId;
+
   res.send({
-    'Answer': message.content,
-	'Token': message.chatId,
+    'Answer': response.content,
+	'Token': new_token,
 	'Account': account,
   });
-  }
+  
+  arr_of_puppets.get(client_key)[1] = false;
+  arr_of_puppets.get(client_key)[2] = false;
+  arr_of_puppets.get(client_key)[0].unauthenticate();
+
 })();
 });
 app.listen(port, () =>{
